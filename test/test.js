e2df4904253820e4e1a81c8e8c901f45865d43fb
Manuel Rauber <manuel.rauber@thinktecture.com>
2016-11-09 11:55:46 -0800

    Cleanup

--- test/test.js
+++ test/test.js
@@ -1,24 +1,26 @@
-var restify  = require('restify');
-var cors     = require('../src/index');
+'use strict';
 
-exports.corsServer = function(corsConfig) {
-  var middleware = cors(corsConfig);
-  var server = restify.createServer();
-  server.pre(middleware.preflight);
-  server.use(middleware.actual);
-  server.get('/test', function(req, res, next) {
-    res.header('Custom-Response-Header', '123456');
-    res.header('Max-Age', 5*60*1000);
-    res.send(200, 'ok');
-    next();
-  });
-  return server;
+var restify = require('restify');
+var cors = require('../lib/index');
+
+exports.corsServer = function (corsConfig) {
+    var middleware = cors(corsConfig);
+    var server = restify.createServer();
+    server.pre(middleware.preflight);
+    server.use(middleware.actual);
+    server.get('/test', function (req, res, next) {
+        res.header('Custom-Response-Header', '123456');
+        res.header('Max-Age', 5 * 60 * 1000);
+        res.send(200, 'ok');
+        next();
+    });
+    return server;
 };
 
-exports.noHeader = function(name) {
-  return function(res) {
-    if (res.headers.hasOwnProperty(name)) {
-      return 'Should not have header ' + name;
-    }
-  };
+exports.noHeader = function (name) {
+    return function (res) {
+        if (res.headers.hasOwnProperty(name)) {
+            return 'Should not have header ' + name;
+        }
+    };
 };
