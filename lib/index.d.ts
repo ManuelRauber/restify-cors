426318b2b86cdd5d733266a451721cd68595c583
Manuel Rauber <manuel.rauber@thinktecture.com>
2016-11-09 12:50:29 -0800

    Updated typings

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -2,14 +2,14 @@
 // Project: restify-cors
 // Definitions by: Manuel Rauber <manuel.rauber@thinktecture.com>
 
-import RestifyCorsMiddleware = RestifyCors.RestifyCorsMiddleware;
+import {RequestHandler} from 'restify';
 
 export = RestifyCors;
 
 declare function RestifyCors(options: RestifyCors.RestifyCorsOptions): RestifyCors.RestifyCorsMiddleware;
 
 declare namespace RestifyCors {
-    type CorsHandlerCallback = (options: RestifyCorsOptions) => Restify.RequestHandler;
+    type CorsHandlerCallback = (options: RestifyCorsOptions) => RequestHandler;
 
     export interface RestifyCorsOptions {
         origins?: Array<string>;
